0cb572ad099de3852ccebabba1844495f0710e3e
spacer730 <hoeshashemi_199@msn.com>
2018-04-08 16:56:28 +0200

    Changed KDE attempt to working Mayavi Example

--- 3D Gaia Density Map/OLDImport.py
+++ 3D Gaia Density Map/OLDImport.py
@@ -2,7 +2,9 @@ import astropy.units as u
 from astropy.coordinates.sky_coordinate import SkyCoord
 from astropy.units import Quantity
 from astropy.table import Table
+from mpl_toolkits.mplot3d import Axes3D
 from scipy import stats
+from sklearn.neighbors import KernelDensity
 from mayavi import mlab
 import astropy.coordinates as asc
 import matplotlib.pyplot as plt
@@ -13,19 +15,19 @@ results = np.array(readresults)
 distances = 1000/results['parallax']
 
 #Convert coordinates to galactic and then to cartesian. 
-#coordinates_ICRS = asc.SkyCoord(ra=results['ra']*u.degree, dec=results['dec']*u.degree, distance=distances*u.pc, frame='icrs', obstime='J2015')
-#coordinates_galactic = coordinates_ICRS.galactic
-#coordinates_cartesian_galactic_converted = np.column_stack((coordinates_galactic.cartesian.x.value, coordinates_galactic.cartesian.y.value, coordinates_galactic.cartesian.z.value))
+coordinates_ICRS = asc.SkyCoord(ra=results['ra']*u.degree, dec=results['dec']*u.degree, distance=distances*u.pc, frame='icrs', obstime='J2015')
+coordinates_galactic = coordinates_ICRS.galactic
+coordinates_cartesian_galactic_converted = np.column_stack((coordinates_galactic.cartesian.x.value, coordinates_galactic.cartesian.y.value, coordinates_galactic.cartesian.z.value))
 
 #Check if their galactic coordinates matches ours 
 realgalactic = np.column_stack((results['l'],results['b'],distances))
-#convertedgalactic = np.column_stack((coordinates_galactic.l.degree,coordinates_galactic.b.degree,coordinates_galactic.distance.pc))
-#differencegalactic = convertedgalactic-realgalactic
+convertedgalactic = np.column_stack((coordinates_galactic.l.degree,coordinates_galactic.b.degree,coordinates_galactic.distance.pc))
+differencegalactic = convertedgalactic-realgalactic
 
 #Check for differences in the cartesian coordinates between using the converted and the real galactic coordinates
 coordinates_cartesian_galactic_real = asc.spherical_to_cartesian(distances*u.pc, results['b']*u.degree, results['l']*u.degree)
 coordinates_cartesian_galactic_real = np.column_stack((coordinates_cartesian_galactic_real[0].value, coordinates_cartesian_galactic_real[1].value, coordinates_cartesian_galactic_real[2].value))
-#differencecartesian = coordinates_cartesian_galactic_real - coordinates_cartesian_galactic_converted
+differencecartesian = coordinates_cartesian_galactic_real - coordinates_cartesian_galactic_converted
 
 x = coordinates_cartesian_galactic_real[:,0]#.filled(0)
 y = coordinates_cartesian_galactic_real[:,1]#.filled(0)
@@ -33,19 +35,20 @@ z = coordinates_cartesian_galactic_real[:,2]#.filled(0)
 
 xyz = np.vstack([x,y,z])
 
-kde = stats.gaussian_kde(xyz)
+d = xyz.shape[0]
+n = xyz.shape[1]
+bw = (n*(d+2)/4.)**(-1./(d+4))
 
-X, Y, Z = np.mgrid[x.min():x.max():100j, y.min():y.max():100j, z.min():z.max():100j]
+kde_fit = KernelDensity(bandwidth=bw, kernel='epanechnikov').fit(xyz.T)
 
-coords = np.vstack([item.ravel() for item in [X, Y, Z]])
-#positions = np.vstack([X.ravel(), Y.ravel(), Z.ravel()])
-density = kde(coords).reshape(X.shape)
-#np.reshape(np.exp(kde_fit.score_samples(positions.T)), X.shape)
+X, Y, Z=np.mgrid[x.min():x.max():100j, y.min():y.max():100j, z.min():z.max():100j]
+positions=np.vstack([X.ravel(), Y.ravel(), Z.ravel()])
+dens = np.reshape(np.exp(kde_fit.score_samples(positions.T)), X.shape)
 
 # Visualize the density estimate as isosurfaces
-#mlab.contour3d(X, Y, Z, density, opacity=0.5)
-#mlab.axes()
-#mlab.show()
+mlab.contour3d(X, Y, Z, dens, opacity=0.5)
+mlab.axes()
+mlab.show()
 
 #plt.plot
 #plt.imshow(np.rot90(dens), cmap=plt.cm.viridis, extent=[x.min(), x.max(), y.min(), y.max(), z.min(), z.max()], aspect='auto')
